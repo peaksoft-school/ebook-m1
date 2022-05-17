9ec99dce6329d384e0e7da2a7deda0a489f41445
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-05-17 18:20:23 +0600

    empty project

---
+++ src/main/java/kg/peaksoft/ebookm1/EbookM1Application.java
@@ -0,0 +1,13 @@
+package kg.peaksoft.ebookm1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookM1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookM1Application.class, args);
+	}
+
+}
