60e889977e02e3b205dba0813fc10d10cd99514f
Gulzhan Bekbolotova <gulzhan.bekbolotova.kk@gmail.com>
2022-05-25 18:47:12 +0300

    testing

--- src/main/java/kg/peaksoft/ebookm1/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package kg.peaksoft.ebookm1.repository;
-
-import kg.peaksoft.ebookm1.entity.book.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
