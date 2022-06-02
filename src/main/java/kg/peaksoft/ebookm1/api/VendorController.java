826fb11ffacb5e7ef893fd106095504875798d8a
Ulukmyrza <ulukmyrza92@gmail.com>
2022-06-02 18:11:52 +0300

    refactor last v12

--- src/main/java/kg/peaksoft/ebookm1/api/VendorController.java
+++ src/main/java/kg/peaksoft/ebookm1/api/VendorController.java
@@ -2,6 +2,8 @@ package kg.peaksoft.ebookm1.api;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import kg.peaksoft.ebookm1.dto.book.BookRequest;
+import kg.peaksoft.ebookm1.dto.promocode.PromocodeRequest;
 import kg.peaksoft.ebookm1.dto.vendor.VendorRequest;
 import kg.peaksoft.ebookm1.dto.vendor.VendorResponse;
 import kg.peaksoft.ebookm1.service.VendorService;
@@ -26,10 +28,52 @@ public class VendorController {
         return service.update(id, request);
     }
 
-    @Operation(summary = "<Method delete by id", description = "User with role ADMIN and VENDOR can deleted")
+    @Operation(summary = "Method delete by id", description = "User with role ADMIN and VENDOR can deleted")
     @PreAuthorize("hasAnyAuthority('ADMIN', 'VENDOR')")
     @DeleteMapping("{id}")
     public VendorResponse deleteById(@PathVariable Long id) {
         return service.deleteById(id);
     }
+    @Operation(summary = "Method to add new book", description = "Vendor can add new book to his profile")
+    @PreAuthorize("hasAnyAuthority('VENDOR')")
+    @PostMapping("/{id}")
+    public VendorResponse addBookToVendor(@PathVariable long id, @RequestBody BookRequest request) {
+        return service.addBookToVendor(request, id);
+    }
+
+    @Operation(summary = "Method to update vendor's book", description = "Vendor can update his books from his book list")
+    @PreAuthorize("hasAnyAuthority('VENDOR')")
+    @PutMapping("/{vendorId}/{bookId}")
+    public VendorResponse updateBook(@PathVariable long vendorId, @PathVariable long bookId, @RequestBody BookRequest request) {
+        return service.updateBookVendor(vendorId, bookId, request);
+    }
+
+    @Operation(summary = "Method to delete book", description = "Vendor and ADMIN can delete book from his book list")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'VENDOR')")
+    @DeleteMapping("/{vendorId}/{bookId}")
+    public VendorResponse deleteBook(@PathVariable long vendorId, @PathVariable long bookId) {
+        return service.deleteBookVendor(vendorId, bookId);
+    }
+
+    @Operation(summary = "Method to add new promocode", description = "Vendor can add new promocode to his profile")
+    @PreAuthorize("hasAnyAuthority('VENDOR')")
+    @PostMapping("/{id}")
+    public VendorResponse addPromoCode(@PathVariable long id, @RequestBody PromocodeRequest promocodeRequest) {
+        return service.addPromocode(promocodeRequest, id);
+    }
+
+    @Operation(summary = "Method to update vendor's promocode", description = "Vendor can update his books from his promocode list")
+    @PreAuthorize("hasAnyAuthority('VENDOR')")
+    @PutMapping("/{vendorId}/{promoCodeId}")
+    public VendorResponse updatePromoCode(@PathVariable long vendorId, @PathVariable long promoCodeId, @RequestBody PromocodeRequest promocodeRequest) {
+        return service.updatePromocode(promocodeRequest, vendorId, promoCodeId);
+    }
+
+    @Operation(summary = "Method to delete book", description = "Vendor and ADMIN can delete promocode from his book list")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'VENDOR')")
+    @DeleteMapping("/{vendorId}/{promoCodeId}")
+    public VendorResponse deletePromocode(@PathVariable long vendorId, @PathVariable long promoCodeId) {
+        return service.deletePromocode(vendorId, promoCodeId);
+    }
+
 }
--- src/main/java/kg/peaksoft/ebookm1/service/VendorService.java
+++ src/main/java/kg/peaksoft/ebookm1/service/VendorService.java
@@ -1,10 +1,18 @@
 package kg.peaksoft.ebookm1.service;
 
+import kg.peaksoft.ebookm1.book.BookEditMapper;
+import kg.peaksoft.ebookm1.dto.book.BookRequest;
+import kg.peaksoft.ebookm1.dto.promocode.PromocodeRequest;
 import kg.peaksoft.ebookm1.dto.vendor.VendorRequest;
 import kg.peaksoft.ebookm1.dto.vendor.VendorResponse;
+import kg.peaksoft.ebookm1.entity.Promocode;
 import kg.peaksoft.ebookm1.entity.User;
+import kg.peaksoft.ebookm1.entity.book.Book;
+import kg.peaksoft.ebookm1.mapper.promocode.PromocodeEditMapper;
 import kg.peaksoft.ebookm1.mapper.vendor.VendorEditMapper;
 import kg.peaksoft.ebookm1.mapper.vendor.VendorViewMapper;
+import kg.peaksoft.ebookm1.repository.BookRepository;
+import kg.peaksoft.ebookm1.repository.PromocodeRepository;
 import kg.peaksoft.ebookm1.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,6 +28,10 @@ public class VendorService {
     private final UserRepository repository;
     private final VendorEditMapper editMapper;
     private final VendorViewMapper viewMapper;
+    private final BookEditMapper bookEditMapper;
+    private final BookRepository bookRepository;
+    private final PromocodeEditMapper promocodeEditMapper;
+    private final PromocodeRepository promocodeRepository;
 
     public VendorResponse create(VendorRequest request) {
         User vendor = editMapper.createVendor(request);
@@ -44,4 +56,71 @@ public class VendorService {
     public List<VendorResponse> getAllVendors() {
         return viewMapper.viewVendors();
     }
+    //  addBook button function section  starts  ==========================================================
+    public VendorResponse addBookToVendor(BookRequest bookRequest, Long id) {
+        Book book = bookEditMapper.createBook(bookRequest);
+        User user = repository.findById(id).get();
+        book.setUser(user);
+        bookRepository.save(book);
+        repository.save(user);
+        return viewMapper.viewVendor(user);
+    }
+
+    public VendorResponse updateBookVendor(Long userId, Long bookId, BookRequest bookRequest) {
+        User user = repository.findById(userId).get();
+        Book book = bookRepository.findById(bookId).get();
+        bookEditMapper.updateBook(book, bookRequest);
+        book.setUser(user);
+        bookRepository.save(book);
+        repository.save(user);
+        return viewMapper.viewVendor(user);
+    }
+
+    public VendorResponse deleteBookVendor(Long userId, Long bookId) {
+        User user = repository.findById(userId).get();
+        Book book = bookRepository.findById(bookId).get();
+        bookRepository.delete(book);
+        repository.save(user);
+        return viewMapper.viewVendor(user);
+    }
+    //  addBook button function section  ends  ==========================================================
+
+
+    // addPromocode section starts   ====================================================
+
+    public VendorResponse addPromocode(PromocodeRequest promocodeRequest, Long id) {
+        Promocode promocode = promocodeEditMapper.create(promocodeRequest);
+        User user = repository.findById(id).get();
+        List<Book> bookList = user.getBooks();
+        for (Book book : bookList
+        ) {
+            book.setPromocode(promocode);
+        }
+        promocode.setUser(user);
+        promocodeRepository.save(promocode);
+        repository.save(user);
+        return viewMapper.viewVendor(user);
+    }
+    public VendorResponse updatePromocode(PromocodeRequest promocodeRequest, Long vendorId, Long promoCodeId) {
+        User user = repository.findById(vendorId).get();
+        Promocode promocode = promocodeRepository.findById(promoCodeId).get();
+        promocodeEditMapper.update(promocode,promocodeRequest);
+        List<Book> bookList = user.getBooks();
+        for (Book book : bookList
+        ) {
+            book.setPromocode(promocode);
+        }
+        promocode.setUser(user);
+        promocodeRepository.save(promocode);
+        repository.save(user);
+        return viewMapper.viewVendor(user);
+    }
+    public VendorResponse deletePromocode(Long vendorId, Long promoCodeId) {
+        User user = repository.findById(vendorId).get();
+        Promocode promocode = promocodeRepository.findById(promoCodeId).get();
+        promocodeRepository.delete(promocode);
+        repository.save(user);
+        return viewMapper.viewVendor(user);
+    }
+
 }
