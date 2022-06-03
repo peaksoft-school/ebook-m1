f7c61eb7345acb1d3180d7a5282fc2fe2fdb5ce5
Ulukmyrza <ulukmyrza92@gmail.com>
2022-06-03 20:30:17 +0300

    refactor last v16 Vendor class deleted

--- src/main/java/kg/peaksoft/ebookm1/dto/user/Vendor.java
+++
@@ -1,9 +0,0 @@
-package kg.peaksoft.ebookm1.dto.user;
-
-public class Vendor {
-    private String firstName;
-    private String lastName;
-    private String phoneNumber;
-    private String email;
-    private String password;
-}
