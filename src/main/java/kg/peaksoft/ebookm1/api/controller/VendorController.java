339e7905c7cd74b7d30680200dcaea66ed3acc01
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-15 14:48:44 +0600

    refactor VendorController

--- src/main/java/kg/peaksoft/ebookm1/api/controller/VendorController.java
+++ src/main/java/kg/peaksoft/ebookm1/api/controller/VendorController.java
@@ -9,17 +9,28 @@ import kg.peaksoft.ebookm1.api.payload.vendor.VendorRequest;
 import kg.peaksoft.ebookm1.api.payload.vendor.VendorResponse;
 import kg.peaksoft.ebookm1.db.services.BookService;
 import kg.peaksoft.ebookm1.db.services.VendorService;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
-@Slf4j
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/vendors")
-@Tag(name = "Vendor", description = "The Vendor API")
+@RequestMapping("api/vendors")
+@PreAuthorize("hasAnyAuthority('ROLE_VENDOR')")
+@Tag(name = "Vendor API", description = "The Vendor endpoints")
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 public class VendorController {
 
@@ -28,7 +39,6 @@ public class VendorController {
 
     // Vendors
     @Operation(summary = "Method update by id", description = "User with role VENDOR can update")
-    @PreAuthorize("hasAnyAuthority('ROLE_VENDOR')")
     @PutMapping("{id}")
     public VendorResponse updateById(@PathVariable Long id, @RequestBody VendorRequest request) {
         log.info("Inside Vendor controller update vendor by id method");
@@ -36,7 +46,7 @@ public class VendorController {
     }
 
     @Operation(summary = "Method delete by id", description = "User with role ADMIN and VENDOR can deleted")
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_VENDOR')")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @DeleteMapping("{id}")
     public VendorResponse deleteById(@PathVariable Long id) {
         log.info("Inside Vendor controller delete vendor by id method");
@@ -45,7 +55,6 @@ public class VendorController {
 
     // Books
     @Operation(summary = "Method to add new book", description = "Vendor can add new book to his profile")
-    @PreAuthorize("hasAnyAuthority('ROLE_VENDOR')")
     @PostMapping("/book/{id}")
     public VendorResponse addBookToVendor(@PathVariable long id, @RequestBody BookRequest request) {
         log.info("Inside Vendor controller add book to vendor method");
@@ -53,7 +62,6 @@ public class VendorController {
     }
 
     @Operation(summary = "Method to update vendor's book", description = "Vendor can update his books from his book list")
-    @PreAuthorize("hasAnyAuthority('ROLE_VENDOR')")
     @PutMapping("/book/{vendorId}/{bookId}")
     public VendorResponse updateBook(@PathVariable long vendorId, @PathVariable long bookId, @RequestBody BookRequest request) {
         log.info("Inside Vendor controller update book by id method");
@@ -61,7 +69,7 @@ public class VendorController {
     }
 
     @Operation(summary = "Method to delete book", description = "Vendor and ADMIN can delete book from his book list")
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_VENDOR')")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @DeleteMapping("/book/{vendorId}/{bookId}")
     public VendorResponse deleteBook(@PathVariable long vendorId, @PathVariable long bookId) {
         log.info("Inside the Vendor controller is a method for deleting their books");
@@ -77,7 +85,6 @@ public class VendorController {
 
     // Promo code
     @Operation(summary = "Method to add new promo code", description = "Vendor can add new promo code to his profile")
-    @PreAuthorize("hasAnyAuthority('ROLE_VENDOR')")
     @PostMapping("/promo/{id}")
     public VendorResponse addPromoCode(@PathVariable long id, @RequestBody PromocodeRequest promocodeRequest) {
         log.info("Inside Vendor controller add promo code to vendor method");
@@ -85,7 +92,6 @@ public class VendorController {
     }
 
     @Operation(summary = "Method to update vendor's promo code", description = "Vendor can update his books from his promo code list")
-    @PreAuthorize("hasAnyAuthority('ROLE_VENDOR')")
     @PutMapping("/promo/{vendorId}/{promoCodeId}")
     public VendorResponse updatePromoCode(@PathVariable long vendorId, @PathVariable long promoCodeId, @RequestBody PromocodeRequest promocodeRequest) {
         log.info("Inside the Vendor controller, update the promo code of the vendor method");
@@ -93,10 +99,11 @@ public class VendorController {
     }
 
     @Operation(summary = "Method to delete book", description = "Vendor and ADMIN can delete promo code from his book list")
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_VENDOR')")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @DeleteMapping("/promo/{vendorId}/{promoCodeId}")
     public VendorResponse deletePromoCode(@PathVariable long vendorId, @PathVariable long promoCodeId) {
         log.info("Inside the Vendor controller, delete the promo code of the vendor method");
         return service.deletePromocode(vendorId, promoCodeId);
     }
+
 }
