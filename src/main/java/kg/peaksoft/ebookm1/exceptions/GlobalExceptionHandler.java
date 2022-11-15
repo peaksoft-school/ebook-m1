c49f610fd60cd8a12b057dab66cb0cfca6ae6b91
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-15 17:27:47 +0600

    refactor GlobalExceptionHandler

--- src/main/java/kg/peaksoft/ebookm1/exceptions/GlobalExceptionHandler.java
+++ src/main/java/kg/peaksoft/ebookm1/exceptions/GlobalExceptionHandler.java
@@ -12,24 +12,24 @@ import java.util.Date;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ExceptionResponse> handleResourceNotFoundException(ResourceNotFoundException exception,
-                                                                             WebRequest request) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),exception.getMessage(),
+    public ResponseEntity<ExceptionResponse> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), exception.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(MyException.class)
     public ResponseEntity<ExceptionResponse> handleMeException(MyException exception, WebRequest request) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),exception.getMessage(),
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), exception.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleGlobalException(Exception exception, WebRequest request) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),exception.getMessage(),
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), exception.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
 }
