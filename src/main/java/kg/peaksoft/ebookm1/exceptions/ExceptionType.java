431595140abeb9b5b615904f93b9c4c635bfb48b
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-15 17:27:03 +0600

    refactor ExceptionType

--- src/main/java/kg/peaksoft/ebookm1/exceptions/ExceptionType.java
+++ src/main/java/kg/peaksoft/ebookm1/exceptions/ExceptionType.java
@@ -2,6 +2,7 @@ package kg.peaksoft.ebookm1.exceptions;
 
 public class ExceptionType {
 
-    public static final String LOGIN_FAILED = "Login_failed";
+    public static final String LOGIN_FAILED = "Login failed";
     public static final String SUCCESSFULLY = "Successfully";
+
 }
