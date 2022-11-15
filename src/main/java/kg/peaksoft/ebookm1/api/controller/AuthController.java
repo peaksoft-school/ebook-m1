d9eb045b656560ef38d8a67a05881a5baf4a2f5f
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-15 14:08:20 +0600

    refactor AuthController

--- src/main/java/kg/peaksoft/ebookm1/api/controller/AuthController.java
+++ src/main/java/kg/peaksoft/ebookm1/api/controller/AuthController.java
@@ -15,20 +15,25 @@ import kg.peaksoft.ebookm1.db.repository.UserRepository;
 import kg.peaksoft.ebookm1.db.services.ClientService;
 import kg.peaksoft.ebookm1.db.services.VendorService;
 import kg.peaksoft.ebookm1.exceptions.ExceptionType;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/public")
+@RequestMapping("api/public")
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
-@Tag(name = "Authentication", description = "User with role ADMIN can authenticate")
+@Tag(name = "Auth API", description = "Registration and authentication")
 public class AuthController {
 
     private final JwtTokenUtil jwtTokenUtil;
@@ -37,8 +42,8 @@ public class AuthController {
     private final VendorService vendorService;
     private final ClientService userService;
 
-    @PostMapping("/login")
-    @Operation(summary = "All users can authenticate", description = "login all users")
+    @Operation(summary = "Login", description = "Only registered users can login")
+    @PostMapping("login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
         try {
             UsernamePasswordAuthenticationToken authenticationToken =
@@ -52,17 +57,18 @@ public class AuthController {
         }
     }
 
-    @Operation(summary = "Method registration vendor", description = "Sign up/sign vendor layout")
-    @PostMapping("/vendor")
+    @Operation(summary = "Register vendor's", description = "Vendor registration")
+    @PostMapping("register/vendor")
     public VendorResponse registration(@RequestBody VendorRequest request) {
         log.info("Inside AuthController registration vendor method");
         return vendorService.create(request);
     }
 
-    @Operation(summary = "Method registration client", description = "Registration client layout")
-    @PostMapping("/client")
+    @Operation(summary = "Register client", description = "Client registration")
+    @PostMapping("register/client")
     public ClientResponse registration(@RequestBody ClientRequest request) {
         log.info("Inside AuthController registration client method");
         return userService.registration(request);
     }
+
 }
