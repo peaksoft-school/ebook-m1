26c619c4166b3c7c727b1f03b30ebb7c971d8dbc
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-16 17:23:54 +0600

    refactor Role entity

--- src/main/java/kg/peaksoft/ebookm1/db/entity/Role.java
+++ src/main/java/kg/peaksoft/ebookm1/db/entity/Role.java
@@ -5,23 +5,31 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.util.List;
 
-@Entity
-@Table(name = "roles")
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
 public class Role {
 
     @Id
     @GeneratedValue(generator = "role_gen", strategy = GenerationType.SEQUENCE)
     @SequenceGenerator(name = "role_gen", sequenceName = "role_seq", allocationSize = 1)
     private Long id;
+
     private String name;
 
     @ManyToMany(mappedBy = "roles")
     private List<User> users;
+
 }
