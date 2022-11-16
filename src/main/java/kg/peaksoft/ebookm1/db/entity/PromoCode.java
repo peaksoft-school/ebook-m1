d6eae5bb290d66fd13a53901e0d767d3346e22a3
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-16 16:02:10 +0600

    refactor PromoCode v2

--- src/main/java/kg/peaksoft/ebookm1/db/entity/PromoCode.java
+++ src/main/java/kg/peaksoft/ebookm1/db/entity/PromoCode.java
@@ -1,20 +1,30 @@
 package kg.peaksoft.ebookm1.db.entity;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.time.LocalDate;
 import java.util.List;
 
-@Entity
-@Table(name = "promocodes")
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "promocodes")
 public class PromoCode {
 
     @Id
