cbdfa80663ab24be93186935bd09272e95b1d314
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-17 16:34:23 +0600

    refactor HistoryOperationRepository

--- src/main/java/kg/peaksoft/ebookm1/db/repository/HistoryOperationRepository.java
+++ src/main/java/kg/peaksoft/ebookm1/db/repository/HistoryOperationRepository.java
@@ -1,12 +1,10 @@
 package kg.peaksoft.ebookm1.db.repository;
 
 import kg.peaksoft.ebookm1.db.entity.HistoryOperation;
-import kg.peaksoft.ebookm1.db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HistoryOperationRepository extends JpaRepository<HistoryOperation, Long> {
 
-    HistoryOperation findByUser(User user);
 }
