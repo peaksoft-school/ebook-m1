d5c7e118fb46bdef9f5aea221228f7385625e4ef
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-17 19:39:36 +0600

    Refactor V2_insertData.sql

--- src/main/resources/db/migration/V2__insertData.sql
+++ src/main/resources/db/migration/V2__insertData.sql
@@ -1,4 +1,8 @@
-insert into users(id,created,email,first_name,is_active,last_name,password,phone_number,basket_id,wish_list_id)
-values(1,'2020-07-14','admin@gmail.com','Admin',true,'Admin','admin',+7977461377,null,null );
-insert into roles(id, name) VALUES (1,'ROLE_ADMIN');
-insert into users_roles(user_id, roles_id) VALUES (1,1);
+INSERT INTO users(id, created, email, first_name, is_active, last_name, password, phone_number, basket_id, wish_list_id)
+VALUES (1, '2020-07-14', 'admin@gmail.com', 'Admin', true, 'Admin', 'admin', +7977461377, null, null);
+
+INSERT INTO roles(id, name)
+VALUES (1, 'ROLE_ADMIN');
+
+INSERT INTO users_roles(user_id, roles_id)
+VALUES (1, 1);
