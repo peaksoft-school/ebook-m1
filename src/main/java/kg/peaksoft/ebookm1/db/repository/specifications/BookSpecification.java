8a246f120996556591b86f3672291dc1357a3642
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-17 16:17:45 +0600

    refactor BookSpecification

--- src/main/java/kg/peaksoft/ebookm1/db/repository/specifications/BookSpecification.java
+++ src/main/java/kg/peaksoft/ebookm1/db/repository/specifications/BookSpecification.java
@@ -6,39 +6,37 @@ import kg.peaksoft.ebookm1.db.enums.TypeOfBook;
 import kg.peaksoft.ebookm1.db.entity.Book;
 import org.springframework.data.jpa.domain.Specification;
 
-import javax.persistence.criteria.*;
+import javax.persistence.criteria.Predicate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BookSpecification  {
+public class BookSpecification {
 
-    public static Specification<Book> getFilter(Genre genreEnum, TypeOfBook typeOfBook){
-        return ((root, query, criteriaBuilder) ->{
+    public static Specification<Book> getFilter(Genre genreEnum, TypeOfBook typeOfBook) {
+        return ((root, query, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
             if (genreEnum != null) {
-              predicates.add(criteriaBuilder.equal(root.get("genre"),genreEnum));
+                predicates.add(criteriaBuilder.equal(root.get("genre"), genreEnum));
             }
             if (typeOfBook != null) {
-                predicates.add(criteriaBuilder.equal(root.get("typeOfBook"),typeOfBook));
+                predicates.add(criteriaBuilder.equal(root.get("typeOfBook"), typeOfBook));
             }
-
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         });
     }
-    public static Specification<Book> getByStatusAndTypeOfBook(Genre genreEnum,TypeOfBook typeOfBook,RequestStatus status){
-        return ((root, query, criteriaBuilder) ->{
-            List<Predicate> predicates = new ArrayList<>();
 
-            if (genreEnum != null) {
-                predicates.add(criteriaBuilder.equal(root.get("genre"),genreEnum));
+    public static Specification<Book> getByStatusAndTypeOfBook(Genre genre, TypeOfBook type, RequestStatus status) {
+        return ((root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            if (genre != null) {
+                predicates.add(criteriaBuilder.equal(root.get("genre"), genre));
             }
-            if (typeOfBook != null) {
-                predicates.add(criteriaBuilder.equal(root.get("typeOfBook"),typeOfBook));
+            if (type != null) {
+                predicates.add(criteriaBuilder.equal(root.get("typeOfBook"), type));
             }
             if (status != null) {
-                predicates.add(criteriaBuilder.equal(root.get("status"),status));
+                predicates.add(criteriaBuilder.equal(root.get("status"), status));
             }
-
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         });
     }
