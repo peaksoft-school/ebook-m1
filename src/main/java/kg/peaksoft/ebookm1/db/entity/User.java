d479be8638c196cb071ec2a03d408bd6a6dcc7ba
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-17 20:00:57 +0600

    Refactor User entity v2

--- src/main/java/kg/peaksoft/ebookm1/db/entity/User.java
+++ src/main/java/kg/peaksoft/ebookm1/db/entity/User.java
@@ -40,7 +40,7 @@ public class User implements UserDetails {
 
     @Id
     @GeneratedValue(generator = "user_gen", strategy = GenerationType.SEQUENCE)
-    @SequenceGenerator(name = "user_gen", sequenceName = "user_seq", allocationSize = 1, initialValue = 2)
+    @SequenceGenerator(name = "user_gen", sequenceName = "user_seq", allocationSize = 1, initialValue = 4)
     private Long id;
 
     @Column(name = "first_name")
