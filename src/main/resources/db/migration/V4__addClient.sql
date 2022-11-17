b05778874172f6228493a58b557b98ce06c7a7ea
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-17 19:50:56 +0600

    Refactor V4_addClient.sql

--- src/main/resources/db/migration/V4__addClient.sql
+++ src/main/resources/db/migration/V4__addClient.sql
@@ -1,4 +1,8 @@
-insert into users(id,created,email,first_name,is_active,last_name,password,phone_number,basket_id,wish_list_id)
-values(4,'2020-07-15','client@gmail.com','Client',true,null,'client',null ,null,null );
-insert into roles(id, name) VALUES (3,'ROLE_CLIENT');
-insert into users_roles(user_id, roles_id) VALUES (4,3);
+INSERT INTO users(id, created, email, first_name, is_active, last_name, password, phone_number, basket_id, wish_list_id)
+VALUES (3, '2020-07-15', 'client@gmail.com', 'Client', true, 'Client', 'client', null, null, null);
+
+INSERT INTO roles(id, name)
+VALUES (3, 'ROLE_CLIENT');
+
+INSERT INTO users_roles(user_id, roles_id)
+VALUES (3, 3);
