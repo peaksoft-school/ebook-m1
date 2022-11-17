28682e0e13d5f485cd4e521b31169161f387d0c1
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-17 17:07:36 +0600

    Refactor NoSuchElementException

--- src/main/java/kg/peaksoft/ebookm1/exceptions/NoSuchElementException.java
+++ src/main/java/kg/peaksoft/ebookm1/exceptions/NoSuchElementException.java
@@ -3,7 +3,7 @@ package kg.peaksoft.ebookm1.exceptions;
 public class NoSuchElementException extends RuntimeException{
 
     public NoSuchElementException(Class<?> clazz, long id) {
-        super(String.format("Entity %s with id not found", clazz.getSimpleName(), id));
+        super(String.format("Entity %s with id %d not found", clazz.getSimpleName(), id));
     }
 
 }
